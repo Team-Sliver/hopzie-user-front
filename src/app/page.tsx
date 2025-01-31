eb84bf0e979935889b75b44e57f7abf439cc49fc
minkithub <atunh9988@naver.com>
2025-01-31 11:42:50 +0900

    redirect 제외

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { RecoilRootProvider } from '../component/recoil-root-provider'
 import { CustomPage } from './[channelNickname]/[uid]/custompage';
 
 export default function Home() {
-    return redirect("https://hopzie.me")
+    // return redirect("https://hopzie.me")
 //   return (
 //    <main>
 //     <RecoilRootProvider>
